63ccc91b64c5dd8761d71e4cabba827256ac52b8
arthurls7 <jals@ic.ufal.br>
2023-08-13 17:42:35 -0300

    repositorio react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Pra quem nunca usou React, olha na <code>src/App.jsx</code> edita o texto e salva pra ver o hot reload.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Grupo 6 - PDS
+      </p>
+    </>
+  )
+}
+
+export default App
