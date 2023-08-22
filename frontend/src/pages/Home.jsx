5f4c73d3f9e0f1e59d34f938661b83d2fa720013
arthurls7 <jals@ic.ufal.br>
2023-08-21 21:54:38 -0300

    Rodar codigo api huxley

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,42 @@
+import React, { useCallback, useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
-import { useCallback } from 'react';
 import { cpp } from '@codemirror/lang-cpp';
+import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Grid from '@mui/material/Grid';
 
 function Home() {
+  const [code, setCode] = useState(`#include <stdio.h>\n\nint main() {\n\tprintf("Hello World!");\n\treturn 0;\n}`);
+
   const onChange = useCallback((value, viewUpdate) => {
-    console.log('value:', value);
+    setCode(value);
   }, []);
 
+  const handleSubmit = () => {
+    console.log(code);
+  };
+
   return (
-    <CodeMirror
-      value={`#include <stdio.h> \n\tint main() { \n\tprintf("Hello World!"); \n\treturn 0;\n}`}
-      height="200px"
-      extensions={cpp()}
-      onChange={onChange}
-    />
+    <Grid container justifyContent="center" alignItems="center" className="h-screen bg-blue-500">
+      <Grid item xs={10} sm={6} md={4}>
+        <div>
+          <h1 className="text-2xl font-bold text-white mb-4 text-center">Bem vindo</h1>
+          <CodeMirror
+            value={code}
+            height="300px"
+            width="100%"
+            extensions={cpp()}
+            onChange={onChange}
+          />
+          <div className="mt-2 text-center">
+            <Button variant="contained" size="small" endIcon={<ArrowForwardIcon />} onClick={handleSubmit}>
+              Submeter
+            </Button>
+          </div>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
-export default Home;
 
+export default Home;
