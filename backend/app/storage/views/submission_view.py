689f47edab06041fc5c70d93b1af0fbf620c0e8f
EliasNsilva <ens@ic.ufal.br>
2023-08-25 00:29:51 -0300

    test submit

--- backend/app/storage/views/submission_view.py
+++ backend/app/storage/views/submission_view.py
@@ -3,18 +3,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class submissionView(APIView):
-    def get(self, request):
+    def post(self, request):
         code = self.request.data['code']
         huxley_login_url = 'https://www.thehuxley.com/api/login'
-        huxley_submission_url = 'https://www.thehuxley.com/api/v1/user/problems/1087/submissions'
+        huxley_submission_url = 'https://www.thehuxley.com/api/v1/user/problems/691/submissions'
         print(code)
-        payload_auth = {
-                        "username": "Elias",
-                        "password": "tardis40"
-                        }
+        payload_auth = {"username": "Elias",
+                        "password": "tardis40"}
 
-        auth = requests.post(huxley_login_url, payload_auth)
-        header = f'{auth["token_type"]} {auth["access_token"]}'
+        auth = requests.post(url=huxley_login_url, json=payload_auth)
+        print("auth ", auth)
+
+        header = {'Authorization' : f'Bearer {auth.json()["token_type"]} {auth.json()["access_token"]}'}
         print("header ", header)
 
         binary_representation = ' '.join(format(byte, '08b') for byte in bytearray(code, 'utf-8'))
