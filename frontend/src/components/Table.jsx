87e87412cd8301f355b68a9b109e183975ef520c
Pedro <phmi@ic.ufal.br>
2023-09-25 19:07:38 -0300

    Tabela inserida
    Tabela inserida

---
+++ frontend/src/components/Table.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
+
+
+const Table = ({ data, columns, itemsPerPage = 15 }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const visibleData = data.slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleFirstPage = () => {
+        setCurrentPage(1);
+    };
+
+    const handleLastPage = () => {
+        setCurrentPage(totalPages);
+    };
+
+  
+    const renderLevelIcon = (level) => {
+        let icon = '';
+        let color = '';
+
+        switch (level) {
+            case 1:
+                icon = '●';
+                color = 'green';
+                break;
+            case 2:
+                icon = '●';
+                color = 'blue';
+                break;
+            case 3:
+                icon = '●';
+                color = 'yellow';
+                break;
+            case 4:
+                icon = '●';
+                color = 'orange';
+                break;
+            case 5:
+                icon = '●';
+                color = 'red';
+                break;
+            default:
+                break;
+        }
+
+        return (
+            <span style={{ color, textAlign: 'center', display: 'block' }}>
+                {icon}
+            </span>
+        );
+    };
+
+   
+    const renderStatusIcon = (status) => {
+        let icon = '';
+        let color = '';
+
+        switch (status) {
+            case 'Completed':
+                icon = <AddTaskIcon />; 
+                color = 'green';
+                break;
+            case 'Failed':
+                icon = <DisabledByDefaultIcon />;
+                color = 'red';
+                break;
+            case 'Pending':
+                icon = '';
+                color = 'white';
+                break;
+            default:
+                break;
+        }
+
+        return (
+            <span style={{ color, textAlign: 'center', display: 'block' }}>
+                {icon}
+            </span>
+        );
+    };
+
+
+
+    return (
+        <div>
+            <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                <thead>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index} style={{ borderBottom: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>
+                                {column.header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {visibleData.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {columns.map((column, columnIndex) => (
+                                <td
+                                    key={columnIndex}
+                                    style={{
+                                        borderBottom: '1px solid #ddd',
+                                        padding: '8px',
+                                        textAlign: column.accessor === 'level' ? 'center' : 'left', // Center the level column
+                                    }}
+                                >
+                                    {column.accessor === 'level'
+                                        ? renderLevelIcon(row[column.accessor])
+                                        : column.accessor === 'status'
+                                            ? renderStatusIcon(row[column.accessor])
+                                            : (
+                                                <Link
+                                                    to={`/otherpage/${row.id}`}
+                                                    style={{
+                                                        textDecoration: 'none',
+                                                        color: 'inherit',
+                                                    }}
+                                                    className="link" 
+                                                >
+                                                    
+                                                    {row[column.accessor]}
+                                                </Link>
+                                            )}
+                                </td>
+                            ))}
+
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                <button onClick={handleFirstPage} disabled={currentPage === 1} style={{ marginRight: '10px' }}><FirstPageIcon /></button>
+                <button onClick={handlePrevPage} disabled={currentPage === 1} style={{ marginRight: '10px' }}><NavigateBeforeIcon /></button>
+                <span style={{ margin: '0 10px' }}>{`Página ${currentPage} de ${totalPages}`}</span>
+                <button onClick={handleNextPage} disabled={currentPage === totalPages} style={{ marginRight: '10px' }}><NavigateNextIcon /></button>
+                <button onClick={handleLastPage} disabled={currentPage === totalPages}><LastPageIcon /></button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Table;
--- frontend/src/pages/Problems.jsx
+++ frontend/src/pages/Problems.jsx
@@ -1,5 +1,97 @@
 import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
+import Table from '../components/Table';
+
+
+
+
+const questions = [
+    { id: 1, level: 1, name: 'Question 1', topics: 'Topic 1, Topic 2', status: 'Failed' },
+    { id: 2, level: 2, name: 'Question 2', topics: 'Topic 3', status: 'Completed' },
+    { id: 3, level: 3, name: 'Question 3', topics: 'Topic 2', status: 'Pending' },
+    { id: 4, level: 1, name: 'Question 4', topics: 'Topic 1, Topic 3', status: 'Completed' },
+    { id: 5, level: 5, name: 'Question 5', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 6, level: 4, name: 'Question 6', topics: 'Topic 3', status: 'Completed' },
+    { id: 7, level: 1, name: 'Question 7', topics: 'Topic 2', status: 'Pending' },
+    { id: 8, level: 5, name: 'Question 8', topics: 'Topic 1, Topic 3', status: 'Completed' },
+    { id: 9, level: 3, name: 'Question 9', topics: 'Topic 2', status: 'Pending' },
+    { id: 10, level: 4, name: 'Question 10', topics: 'Topic 1, Topic 2', status: 'Completed' },
+    { id: 11, level: 2, name: 'Question 11', topics: 'Topic 3', status: 'Pending' },
+    { id: 12, level: 3, name: 'Question 12', topics: 'Topic 2', status: 'Completed' },
+    { id: 13, level: 1, name: 'Question 13', topics: 'Topic 1, Topic 3', status: 'Pending' },
+    { id: 14, level: 2, name: 'Question 14', topics: 'Topic 2', status: 'Completed' },
+    { id: 16, level: 3, name: 'Question 15', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 17, level: 4, name: 'Question 16', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 18, level: 4, name: 'Question 17', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 19, level: 4, name: 'Question 18', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 20, level: 4, name: 'Question 19', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 21, level: 4, name: 'Question 20', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 22, level: 4, name: 'Question 21', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 23, level: 4, name: 'Question 22', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 24, level: 4, name: 'Question 23', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 25, level: 4, name: 'Question 24', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 1, level: 1, name: 'Question 1', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 2, level: 2, name: 'Question 2', topics: 'Topic 3', status: 'Completed' },
+    { id: 3, level: 3, name: 'Question 3', topics: 'Topic 2', status: 'Pending' },
+    { id: 4, level: 1, name: 'Question 4', topics: 'Topic 1, Topic 3', status: 'Completed' },
+    { id: 5, level: 2, name: 'Question 5', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 6, level: 3, name: 'Question 6', topics: 'Topic 3', status: 'Completed' },
+    { id: 7, level: 1, name: 'Question 7', topics: 'Topic 2', status: 'Pending' },
+    { id: 8, level: 2, name: 'Question 8', topics: 'Topic 1, Topic 3', status: 'Completed' },
+    { id: 9, level: 3, name: 'Question 9', topics: 'Topic 2', status: 'Pending' },
+    { id: 10, level: 1, name: 'Question 10', topics: 'Topic 1, Topic 2', status: 'Completed' },
+    { id: 11, level: 2, name: 'Question 11', topics: 'Topic 3', status: 'Pending' },
+    { id: 12, level: 3, name: 'Question 12', topics: 'Topic 2', status: 'Completed' },
+    { id: 13, level: 1, name: 'Question 13', topics: 'Topic 1, Topic 3', status: 'Pending' },
+    { id: 14, level: 2, name: 'Question 14', topics: 'Topic 2', status: 'Completed' },
+    { id: 16, level: 3, name: 'Question 15', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 17, level: 4, name: 'Question 16', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 18, level: 4, name: 'Question 17', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 19, level: 4, name: 'Question 18', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 20, level: 4, name: 'Question 19', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 21, level: 4, name: 'Question 20', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 22, level: 4, name: 'Question 21', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 23, level: 4, name: 'Question 22', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 24, level: 4, name: 'Question 23', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 25, level: 4, name: 'Question 24', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 1, level: 1, name: 'Question 1', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 2, level: 2, name: 'Question 2', topics: 'Topic 3', status: 'Completed' },
+    { id: 3, level: 3, name: 'Question 3', topics: 'Topic 2', status: 'Pending' },
+    { id: 4, level: 1, name: 'Question 4', topics: 'Topic 1, Topic 3', status: 'Completed' },
+    { id: 5, level: 2, name: 'Question 5', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 6, level: 3, name: 'Question 6', topics: 'Topic 3', status: 'Completed' },
+    { id: 7, level: 1, name: 'Question 7', topics: 'Topic 2', status: 'Pending' },
+    { id: 8, level: 2, name: 'Question 8', topics: 'Topic 1, Topic 3', status: 'Completed' },
+    { id: 9, level: 3, name: 'Question 9', topics: 'Topic 2', status: 'Pending' },
+    { id: 10, level: 1, name: 'Question 10', topics: 'Topic 1, Topic 2', status: 'Completed' },
+    { id: 11, level: 2, name: 'Question 11', topics: 'Topic 3', status: 'Pending' },
+    { id: 12, level: 3, name: 'Question 12', topics: 'Topic 2', status: 'Completed' },
+    { id: 13, level: 1, name: 'Question 13', topics: 'Topic 1, Topic 3', status: 'Pending' },
+    { id: 14, level: 2, name: 'Question 14', topics: 'Topic 2', status: 'Completed' },
+    { id: 16, level: 3, name: 'Question 15', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 17, level: 4, name: 'Question 16', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 18, level: 4, name: 'Question 17', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 19, level: 4, name: 'Question 18', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 20, level: 4, name: 'Question 19', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 21, level: 4, name: 'Question 20', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 22, level: 4, name: 'Question 21', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 23, level: 4, name: 'Question 22', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 24, level: 4, name: 'Question 23', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    { id: 25, level: 4, name: 'Question 24', topics: 'Topic 1, Topic 2', status: 'Pending' },
+    // Add more items here as needed
+  ];
+
+
+const columns = [
+    { header: '', accessor: 'level' },
+    { header: 'Nome', accessor: 'name' },
+    { header: 'Tópicos', accessor: 'topics' },
+    { header: 'Status', accessor: 'status' },
+];
+
+
+
+
 
 const Problems = () => {
     const [showTopics, setShowTopics] = useState(false);
@@ -100,18 +192,7 @@ const Problems = () => {
                             <option value="expert">Expert</option>
                         </select>
                     </div>
-                    <div style={{ marginTop: '20px', backgroundColor: '#e3e3e3', padding: '10px', marginLeft: '20px' }}>
-                        <h3>Idioma</h3>
-                        <select
-                            style={{
-                                width: '100%',
-                                height: '30px',
-                            }}
-                        >
-                            <option value="portugues">Português</option>
-                            <option value="ingles">Inglês</option>
-                        </select>
-                    </div>
+                   
                     <div style={{ marginTop: '20px', backgroundColor: '#e3e3e3', padding: '10px', marginLeft: '20px' }}>
                         <h3>Listar</h3>
                         <select
@@ -127,9 +208,16 @@ const Problems = () => {
 
                 </Grid>
 
-                {/* Grid da Direita com largura maior que ta incompleto*/}
-                <Grid item xs={12} sm={8} style={{ backgroundColor: '#e3e3e3', height: '80vh', paddingLeft: '10px', border: '1px dotted #233142' }}>
-                    {/* Conteúdo do Grid 2 */}
+                <Grid item xs={12} sm={8} style={{ backgroundColor: '#e3e3e3', height: '80vh', border: '1px dotted #233142', justifyContent: 'center', alignItems: 'center' }}>
+                    <div style={{ height: '100%', overflow: 'auto', backgroundColor: '#e3e3e3' }}>
+                        <Table data={questions} columns={columns} />
+                    </div>
+
+
+
+
+                    {/* Conteúdo do Grid 2 (restante do espaço) */}
+                    {/* ... Conteúdo do Grid 2 aqui ... */}
                 </Grid>
             </Grid>
         </Grid>
